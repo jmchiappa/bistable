3b2ced87fb52bbdb87c4a8ecc206ca34874a4d9d
Jean-Marc <jm.chiappa@gmail.com>
2019-12-22 16:22:55 +0100

    Suppression des traces de debug

--- Bistable.cpp
+++ Bistable.cpp
@@ -19,7 +19,7 @@ void Bistable::begin(uint8_t Pin,uint8_t Active_Edge, uint8_t Input_type, uint8_
 	Last_input_state = digitalRead(this->PinNumber);
 	t0=0;
 	Update=false;
-	Serial.begin(115200);
+//	Serial.begin(115200);
 }
 
 /* Return true if an expected transition occurs
@@ -28,12 +28,12 @@ bool Bistable::isChanged(void)
 {
 	bool ret=false;
 	uint8_t state = digitalRead(this->PinNumber);
-	Serial.print("1 "+String(Last_input_state)+" "+String(state)+" "+String(Active_Edge));
+//	Serial.print("1 "+String(Last_input_state)+" "+String(state)+" "+String(Active_Edge));
 	if(t0==0) {
 		if(state!=Last_input_state) { // transition occurs
-			Serial.print("-> 2 "+String(Last_input_state)+" "+String(state)+" "+String(Active_Edge));
+//			Serial.print("-> 2 "+String(Last_input_state)+" "+String(state)+" "+String(Active_Edge));
 			if(state==Active_Edge) {
-				Serial.print("-> 3 "+String(Last_input_state)+" "+String(state));
+//				Serial.print("-> 3 "+String(Last_input_state)+" "+String(state));
 				t0=millis();
 			}
 			else {
@@ -43,17 +43,17 @@ bool Bistable::isChanged(void)
 	}
 	else if((millis()-t0)>delay) {
 		if(state!=Last_input_state) { // transition occurs
-			Serial.print("-> 4");
+//			Serial.print("-> 4");
 			Current_state=(Current_state==HIGH?LOW:HIGH);	// XOR Current_state
 			ret=true;	// transition occurs
 		}
-		Serial.print("-> 5");
+//		Serial.print("-> 5");
 		t0=0;
 		Last_input_state = state;
 	}
 
 	//Last_input_state = state;
-	Serial.println();
+//	Serial.println();
 	Update=true;
 	return ret; // no change
 }
